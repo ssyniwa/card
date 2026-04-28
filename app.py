067cb6e7072e02d0b6661e5d9a02c369dc314f21
SyouyaNiwa <109267191+ssyniwa@users.noreply.github.com>
2026-04-28 18:16:37 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -87,15 +87,15 @@ elif st.session_state.page == "REGISTER":
 # 3. デッキ選択画面
 elif st.session_state.page == "SELECT_DECK":
     st.title("🎮 デッキ選択")
-    deck_files =load_deck_names()  # スプレッドシートからデッキ名を取得
-    
+    deck_files = load_deck_names()  # スプレッドシートからデッキ名を取得
+
     selected_file = st.selectbox("デッキ選択", deck_files)
     if st.button("バトル開始！"):
-        my_deck = conn.read(worksheet=selected_file)
+        my_deck = conn.read(worksheet=selected_file)  # 選択したデッキのカードデータを読み込む
         if len(my_deck) < 3:
             st.error("カードは3枚以上登録してください！")
         else:
-            st.session_state.full_deck = my_deck.to_dict(orient="records") # デッキ全体をセッションに保存
+            st.session_state.full_deck = my_deck # デッキ全体をセッションに保存
             st.session_state.player_hp = 300
             st.session_state.cpu_hp = 300
             # 最初の3枚をドロー
